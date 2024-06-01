20bcb5248710918a14eaee5416270530d6aa76c9
FelipeFerro1996 <lipe_ferro96@hotmail.com>
2024-06-01 11:05:02 -0300

    Adicionado model e migrations lista de compra

---
+++ app/Models/ListaCompra.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListaCompra extends Model
+{
+    use HasFactory;
+
+    public $table = 'lista_compras';
+}
---
+++ app/Models/ListaCompraItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListaCompraItem extends Model
+{
+    use HasFactory;
+
+    public $table = 'lista_compras_itens';
+}
